0c85e5322e658bf11f93ee7cbdfd66bea42371a0
Mark Khramko <markkhramko@gmail.com>
2018-11-09 01:33:48 +0700

    Layout fix

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-window.document.onload = function(e){ 
+const rightControls = document.getElementsByClassName("ytp-right-controls");
+alert(rightControls.length);
+if(rightControls.length > 0){	
 
-	const rightControls = document.getElementsByClassName("ytp-right-controls");
-	if(rightControls.length > 0){	
+	const btn = document.createElement("button");
+	btn.className = "ytp-button goposmotrim-button";
+	btn.setAttribute("aria-pressed", false);
+	btn.setAttribute("title", "GoPosmotrim");
 
-		const btn = document.createElement("button");
-		btn.className = "ytp-button goposmotrim-button";
-		btn.setAttribute("aria-pressed", false);
-		btn.setAttribute("title", "GoPosmotrim");
+	const btnSpan = document.createElement("span");
+	btnSpan.className = "goposmotrim-button-content";
+	btnSpan.innerHTML = 'Go!';
+	btn.appendChild(btnSpan);
 
-		const btnSpan = document.createElement("span");
-		btnSpan.className = "goposmotrim-button-content";
-		btnSpan.innerHTML = 'Go!';
-		btn.appendChild(btnSpan);
+	const insertionTarget = rightControls[0];
+	insertionTarget.insertAdjacentElement('afterbegin', btn);
 
-		const insertionTarget = rightControls[0];
-		insertionTarget.insertAdjacentElement('afterbegin', btn);
+	btn.onclick = function(){
+		const urlParams = new URLSearchParams(window.location.search);
+		const videoId = urlParams.get('v');
 
-		btn.onclick = function(){
-			const urlParams = new URLSearchParams(window.location.search);
-			const videoId = urlParams.get('v');
-
-			window.location.href = "https://goposmotrim.herokuapp.com/?v=" + videoId;
-		};
-	}
+		window.location.href = "https://goposmotrim.herokuapp.com/?v=" + videoId;
+	};
 }
